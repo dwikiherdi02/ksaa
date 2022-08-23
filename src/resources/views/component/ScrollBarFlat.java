90cac79ced3127066dbc8b377b1b04dd61575db8
Dwiki Herdiansyah <dwikiherdi520@gmail.com>
2022-08-23 13:05:01 +0700

    Update scrollbar

--- src/resources/views/component/ScrollBarFlat.java
+++ src/resources/views/component/ScrollBarFlat.java
@@ -9,7 +9,9 @@ public class ScrollBarFlat extends JScrollBar {
     public ScrollBarFlat() {
         setUI(new ModernScrollBarUI());
         setPreferredSize(new Dimension(5, 5));
+//        setBackground(new Color(72, 162, 61));
         setBackground(new Color(242, 242, 242));
+//        setForeground(new Color(72, 112, 58));
         setUnitIncrement(20);
     }
 }
